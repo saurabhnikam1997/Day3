e66119df59ca2dfe18be53d72a7f96b20dd4c3c8
saurabhnikam1997 <nikam280197@gmail.com>
2020-09-29 20:25:18 +0530

    assignment

--- file.sh
+++ file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-read -p "Enter value of x: " x
-read -p "Enter value of y: " y
+read -p "Enter value of variable x: " x
+read -p "Enter value of variable y: " y
 Z= expr $x + $y
 
 echo $z
