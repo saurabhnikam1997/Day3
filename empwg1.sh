c19c1a5f8bc01653a3d5ab167576777f927926ee
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-01 18:47:02 +0530

    modify

---
+++ empwg1.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -x
+
+#constant
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
+WAGE_PER_HRS=20
+
+#variable
+randomCheck=$((RANDOM%3))
+
+if [[ $randomCheck=$IS_PRESENT_FULL_TIME ]]
+then
+	emphr=
+
+elif
+then
+
+else
+
+fi
+
