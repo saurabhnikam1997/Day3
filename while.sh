f6152d97a2e5f211a6a551712eb413f0d1a011aa
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-03 19:54:30 +0530

    conflictmodify

--- while.sh
+++ while.sh
@@ -6,17 +6,19 @@ IS_PRESENT_PART_TIME=2
 WAGE_PER_HRS=20
 
 #variable
-randomCheck=$((RANDOM%3))
+while [ randomCheck=$((RANDOM%3)) ]
+do
+      if [[$randomCheck=$IS_PRESENT_FULL_TIME ]]
 
-if [[ $randomCheck=$IS_PRESENT_FULL_TIME ]]
-then
-	emphr=8
+        emphr=8
 
 elif [[ $randomCheck=$IS_PRESENT_FULL_TIME ]]
 then
-	emphr=4
+        emphr=4
 else
-	emphr=0
+        emphr=0
 fi
 salary=$((emphr*WAGE_PER_HRS))
 echo "The salary is $salary"
+
+
