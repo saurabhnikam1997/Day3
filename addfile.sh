3e5376f4e75ae20a41aecc3491d79cb959a465a7
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-03 20:16:17 +0530

    conflict2

--- addfile.sh
+++ addfile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 
-read -p "Enter value of first x: " x
-read -p "Enter value of second y: " y
+read -p "Enter value of first variable x: " x
+read -p "Enter value of second variable y: " y
 Z= expr $x + $y
 
 echo "Result is" $z
